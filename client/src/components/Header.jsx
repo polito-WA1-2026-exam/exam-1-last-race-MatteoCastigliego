bc1fc5f04f42f4187f6aa7580e606d5e28df1d75
MatteoCastigliego <s314196@studenti.polito.it>
2026-06-04 11:11:01 +0200

    feat: added header and footer elements

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+import { Button, Container, Navbar, Nav } from "react-bootstrap";
+import { Link, useNavigate } from 'react-router';
+import UserContext from '../contexts/UserContext.js';
+import { doLogout } from '../api/auth.js';
+
+
+function Header({ doLogout: handleLogout }) {
+  const user = useContext(UserContext);
+
+  const logout = async () => {
+    await doLogout();
+    handleLogout();
+  };
+
+  return (
+    <Navbar bg='dark' variant='dark'>
+      <Container fluid>
+        <Navbar.Brand><Link to='/' style={{ color: 'white', textDecoration: 'none' }}>Last Race</Link></Navbar.Brand>
+        <Nav className="ms-auto">
+          {user.id ? (
+            <>
+              <Nav.Link as={Link} to='/home'>Home</Nav.Link>
+              <Nav.Link as={Link} to='/ranking'>Ranking</Nav.Link>
+              <UserInfo name = {user.name} />
+              <LogoutButton logout = {logout}/>
+            </>
+          ) : (
+            <LoginButton />
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+function LoginButton(){
+  const navigate = useNavigate();
+  return <Button variant='outline-light' onClick={() => navigate('/login')}>Login</Button>
+}
+
+function LogoutButton(prop){
+  return <Button variant='outline-light' onClick={prop.logout}>Logout</Button>
+}
+
+function UserInfo(props){
+  return <Nav.Item className="d-flex align-items-center text-white me-3">{props.name}</Nav.Item>
+}
+
+export default Header;
